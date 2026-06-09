b74b1290589b8615e207e5037989eca168736f80
Chocoatado <ytzlq717@gmail.com>
2026-06-09 11:06:41 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -588,6 +588,7 @@ window.onload = function(){
 	addSound("ding","audio/ding.mp3");
 	addSound("rewind","audio/rewind.mp3");
 	addSound("jazz","audio/jazz.mp3");
+	addSound("bgm","audio/bgm.mp3");
 	addSound("step","audio/step.mp3");
 	addSound("unlock","audio/unlock.mp3");
 	addSound("error","audio/error.mp3");
@@ -618,10 +619,16 @@ window.onload = function(){
 
 				if(STAGE==3 && CURRENT_LEVEL==1){
 					var framesLeft = (rewindLevel.frames.length-rewindFrame) + levelObjects[2].frames.length;
-					if(framesLeft<135){
-						window.HAS_PLAYED_JAZZ = true;
-						createjs.Sound.play("jazz");
-					}
+if(framesLeft<135){
+	window.HAS_PLAYED_JAZZ = true;
+
+	if(!window.BIRTHDAY_BGM){
+		window.BIRTHDAY_BGM = createjs.Sound.play("bgm", {
+			loop: -1,
+			volume: 0.16
+		});
+	}
+}
 				}
 
 			}
