3b6e5d1825eeeed1ab943269f18299dd13063e6e
Chocoatado <ytzlq717@gmail.com>
2026-06-11 17:25:05 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -777,15 +777,17 @@ function iHeartYou(){
 	
 	var can_cont_text = document.getElementById("canvas_container_text");
 
-var vtext = document.getElementById("valentines_text");
-vtext.style.display = "block";
+	var vtext = document.getElementById("valentines_text");
+	vtext.style.display = "block";
+	if(window.location.hash){
+		vtext.textContent = encryptString(decodeURIComponent(window.location.hash).substring(1));
+	}else{
+		vtext.textContent = "a lovely message from me to you <3";
+	}
 
-if(window.location.hash){
-	vtext.textContent = encryptString(decodeURIComponent(window.location.hash).substring(1));
-}else{
-	vtext.innerHTML = "嘿嘿 surprise！&lt;3<br>有没有想起我们一起玩双影奇境的时候，那时你也创作了一幅大作！<br>好的游戏要留给对的人，我宣布这个游戏只对你开放！";
-}
-
+	setTimeout(function(){
+		vtext.style.letterSpacing = "3px";
+	},10);
 
 	// After 9 seconds, swipe down to CREDITS.
 	// No replay. Fuck it.
